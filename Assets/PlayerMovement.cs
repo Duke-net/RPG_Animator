4c36c873d2cdb0fcc28b0dceac273b98e5a52523
Duke <dukenet.ovh@gmail.com>
2020-12-21 01:33:54 +0100

    Fin #1

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] GameObject[] skillParticles;
+    private IEnumerator coroutine;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-    }
 
+        for(int i=0; i < skillParticles.Length; i++)
+        {
+            skillParticles[i].SetActive(false);
+        }
+    }
 
     void Update()
     {
@@ -27,12 +34,46 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("Sprint", false);
         }
+
+        if (Input.GetKeyUp(KeyCode.Alpha1)) UseSkill(1);
+        if (Input.GetKeyUp(KeyCode.Alpha2)) UseSkill(2);
+        if (Input.GetKeyUp(KeyCode.Alpha3)) UseSkill(3);
+        if (Input.GetKeyUp(KeyCode.Alpha4)) UseSkill(4);
+    }
+
+    void UseSkill(int skillNumber)
+    {
+        animator.SetTrigger("Skill" + skillNumber);
+        skillParticles[skillNumber - 1].SetActive(true);
+
+        switch (skillNumber)
+        {
+            case 1:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 5f);
+                break;
+            case 2:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 2f);
+                break;
+            case 3:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 3.5f);
+                break;
+            case 4:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 3f);
+                break;
+        }
+
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator WaitToEnableObject(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        obj.SetActive(false);
+
     }
 
     public void Jump()
     {
         animator.SetTrigger("Jump");
     }
-
-
 }
